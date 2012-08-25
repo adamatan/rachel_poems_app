62356e86e3f779a57c60fbec7fc1dd245585a2ef
Adam Matan <adam@matan.name>
2012-08-25 17:22:02 +0300

    Button on MainActivity now links to the PoemActivity

--- gen/name/matan/rachel_apps/R.java
+++ gen/name/matan/rachel_apps/R.java
@@ -15,8 +15,9 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f070001;
-        public static final int menu_settings=0x7f070002;
+        public static final int menu_settings=0x7f070003;
+        public static final int poemView=0x7f070002;
+        public static final int show_poem_button=0x7f070001;
         public static final int textView1=0x7f070000;
     }
     public static final class layout {
@@ -30,7 +31,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
-        public static final int show_poem_button=0x7f040004;
+        public static final int show_poem_button_text=0x7f040004;
         public static final int title_activity_main=0x7f040003;
     }
     public static final class style {
--- src/name/matan/rachel_apps/MainActivity.java
+++ src/name/matan/rachel_apps/MainActivity.java
@@ -2,7 +2,11 @@ package name.matan.rachel_apps;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,13 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button showPoemButton = (Button) findViewById(R.id.show_poem_button);
+        showPoemButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, PoemActivity.class));
+			}
+		});
     }
 
     @Override
