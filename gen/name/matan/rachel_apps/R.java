faeae937a90876efb3f6615fb3d1df6c57f76f20
Adam Matan <adam@matan.name>
2012-08-27 07:15:56 +0300

    Removed splash screen borders

--- gen/name/matan/rachel_apps/R.java
+++ gen/name/matan/rachel_apps/R.java
@@ -13,16 +13,19 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int rachel=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070003;
-        public static final int poemView=0x7f070002;
+        public static final int imageView1=0x7f070002;
+        public static final int menu_settings=0x7f070004;
+        public static final int poemView=0x7f070003;
         public static final int show_poem_button=0x7f070001;
         public static final int textView1=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int poem_activity=0x7f030001;
+        public static final int activity_splash=0x7f030001;
+        public static final int poem_activity=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
@@ -35,6 +38,7 @@ public final class R {
         public static final int main_activity_title=0x7f040005;
         public static final int menu_settings=0x7f040002;
         public static final int poem_filename_key=0x7f040006;
+        public static final int rachel_splash_description=0x7f040007;
         public static final int show_poem_button_text=0x7f040004;
         public static final int title_activity_main=0x7f040003;
     }
--- src/name/matan/rachel_apps/SplashScreenActivity.java
+++ src/name/matan/rachel_apps/SplashScreenActivity.java
@@ -4,14 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 
 public class SplashScreenActivity extends Activity {
 	protected static final long SPLASH_DELAY = 5000;
 	@Override
 	public void onCreate(Bundle savedInstanceData) {
 		super.onCreate(savedInstanceData);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_splash);
-		
 		Handler h = new Handler();
 		
 		Runnable r = new Runnable() {
